215b86b96f26cda5de54dfd6edd3908a45409e83
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2017-12-30 02:56:35 +0530

    Fixing Navbar Transparency on reload

--- public_static/js/index.js
+++ public_static/js/index.js
@@ -1,7 +1,14 @@
 $(()=>{
     const navbar = $('.navbar');
-    navbar.addClass('transparent');
-    navbar.removeClass('color-dark');
+
+    if($(window).scrollTop() + $('.navbar').height() > ($(window).height()/3)) {
+        navbar.addClass('color-dark');
+        navbar.removeClass('transparent');
+    }
+    else {
+        navbar.addClass('transparent');
+        navbar.removeClass('color-dark');
+    }
 
 
     $(window).scroll(function() {
